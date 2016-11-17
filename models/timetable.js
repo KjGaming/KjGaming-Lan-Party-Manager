34413922f058c82bf1b1a92401b1c609a8f98d6c
Dirk Müller <dirk89@gmx.de>
2016-11-17 10:00:08 +0100

    add Timetable Backend

--- models/timetable.js
+++ models/timetable.js
@@ -4,7 +4,8 @@ var Schema = mongoose.Schema;
 var schema = new Schema({
     title: {type: String, required: true},
     time: {type: String, required: true},
-    mode: {type: Number, required: true} // 0 = LAN , 1 = Tournaments, 2 = Outhers
+    mode: {type: Number, required: true}, // 0 = LAN , 1 = Tournaments, 2 = Outhers
+    content: {type: String}
 });
 
 module.exports = mongoose.model('timetable', schema);
