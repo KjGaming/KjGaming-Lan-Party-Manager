85936eac4e2c97105f2e3c01a53a303d364e720f
Dirk Müller <dirk89@gmx.de>
2016-12-12 16:19:58 +0100

    change the router.use with the correct functions for user, admin and moderator. Also fix the jwt token, only pass with the header.

--- routes/server.js
+++ routes/server.js
@@ -5,11 +5,21 @@ var jwt = require('jsonwebtoken');
 
 router.use('/', function (req, res, next) {
 
-    jwt.verify(req.query.id_token || req.get('Authorization'), '20Kj!G!aming?Rock.17' || '20Kj!G!aming?Rock.Creator.17' || '20Kj!G!aming?Rock.Admin.17', function (err, decoded) {
+    jwt.verify(req.get('Authorization'), '20Kj!G!aming?Rock.17', function (err, decoded) {
         if (err) {
-            return res.status(401).json({
-                title: 'Not Authenticated',
-                error: err
+            jwt.verify(req.get('Authorization'), '20Kj!G!aming?Rock.Creator.17', function (err2, decoded2) {
+                if (err2) {
+                    jwt.verify(req.get('Authorization'), '20Kj!G!aming?Rock.Admin.17', function (err3, decoded3) {
+                        if (err3) {
+                            return res.status(401).json({
+                                title: 'Not Authenticated',
+                                error: err
+                            });
+                        }
+
+                    });
+
+                }
             });
         }
 
