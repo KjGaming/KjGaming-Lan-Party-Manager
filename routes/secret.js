7cdf897bacf395779b79159a433733e746b43f98
Dirk Müller <geninone@gmail.com>
2017-02-02 13:47:59 +0100

    change the gitignore for the secret.js

--- routes/secret.js
+++ routes/secret.js
@@ -1,9 +1,9 @@
 module.exports = {
-    adminSecret: "password1",
-    orgaSecret: "password2",
-    userSecret: "password3",
-    adminTokenSecret: "2",
-    orgaTokenSecret: "1",
+    adminSecret: "a",
+    orgaSecret: "b",
+    userSecret: "V",
+    adminTokenSecret: "1",
+    orgaTokenSecret: "2",
     userTokenSecret: "0",
-    passwordEmail: 'passwordEmail'
+    passwordEmail: 'asdasd'
 };
