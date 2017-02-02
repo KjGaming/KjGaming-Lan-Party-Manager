53f65c30078059dc5d1f036115f2839655e98515
Dirk Müller <geninone@gmail.com>
2017-02-02 16:23:13 +0100

    change Server Layout with ON and OFF

--- src/app/pages/server/server.component.ts
+++ src/app/pages/server/server.component.ts
@@ -35,4 +35,12 @@ export class ServerComponent implements OnInit {
             () => console.log('done loading news')
         );
     }
+
+    isClassVisible(status){
+        if(status == true){
+            return 'table-success';
+        }else{
+            return 'table-danger';
+        }
+    }
 }
