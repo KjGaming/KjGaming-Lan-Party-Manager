b8ee39376d5f62c39d7cf13fa6a9c959f429e8b7
Dirk Müller <geninone@gmail.com>
2017-02-10 17:44:38 +0100

    Layout and function changes on the swiss System

--- src/app/pages/tournament/components/swiss/swiss.component.ts
+++ src/app/pages/tournament/components/swiss/swiss.component.ts
@@ -5,6 +5,7 @@ import {ActivatedRoute} from "@angular/router";
 import {FormBuilder, Validators, FormGroup} from '@angular/forms';
 import {NotificationsService} from "angular2-notifications/src/notifications.service";
 import {BaTournamentService} from "../../../../theme/services/baTournament/baTournament.service";
+import {BaClanService} from "../../../../theme/services/baClan/baClan.service";
 
 @Component({
     selector: 'swiss',
@@ -20,99 +21,149 @@ export class SwissComponent implements OnInit {
 
     @ViewChild('match') match: ModalComponent;
 
-    swissSubscriber:number  = 16;
-    swissResult = [2,1,0,2,3,4,5,1];
+    swissSubscriber: number = 16;
+    swissResult = [2, 1, 0, 2, 3, 4, 5, 1];
     swissResultTable = [
         {
-            'name' : 'Team1',
-            'win' : 2,
-            'loose' : 1,
+            'name': 'Team1',
+            'win': 2,
+            'loose': 1,
             'points': 1.5,
-            'qualified' : 0
+            'qualified': 0
         },
         {
-            'name' : 'Team2',
-            'win' : 0,
-            'loose' : 0,
+            'name': 'Team2',
+            'win': 0,
+            'loose': 0,
             'points': 0,
-            'qualified' : 0
+            'qualified': 0
         },
         {
-            'name' : 'Team3',
-            'win' : 1,
-            'loose' : 0,
+            'name': 'Team3',
+            'win': 1,
+            'loose': 0,
             'points': 1,
-            'qualified' : 0
+            'qualified': 0
         },
         {
-            'name' : 'Team4',
-            'win' : 2,
-            'loose' : 0,
+            'name': 'Team4',
+            'win': 2,
+            'loose': 0,
             'points': 2,
-            'qualified' : 0
+            'qualified': 0
         },
         {
-            'name' : 'Team5',
-            'win' : 1,
-            'loose' : 1,
+            'name': 'Team5',
+            'win': 1,
+            'loose': 1,
             'points': 0.5,
-            'qualified' : 0
+            'qualified': 0
         },
         {
-            'name' : 'Team6',
-            'win' : 0,
-            'loose' : 0,
+            'name': 'Team6',
+            'win': 0,
+            'loose': 0,
             'points': 0,
-            'qualified' : 0
+            'qualified': 0
         },
         {
-            'name' : 'Team7',
-            'win' : 1,
-            'loose' : 2,
+            'name': 'Team7',
+            'win': 1,
+            'loose': 2,
             'points': 0,
-            'qualified' : 0
+            'qualified': 0
         },
         {
-            'name' : 'Team8',
-            'win' : 0,
-            'loose' : 2,
+            'name': 'Team8',
+            'win': 0,
+            'loose': 2,
             'points': -1,
-            'qualified' : 0
+            'qualified': 0
         }
 
     ];
-    swissRound:Object  = ['Runde 1','Runde 2','Runde 3'];
-    swissMatch:Object = {
-        "Runde 1":[
+    swissRound: Object = ['Runde 1', 'Runde 2', 'Runde 3'];
+    swissMatch: Object = {
+        "Runde 1": [
             {
                 "team1": "Team 1",
-                "team2": "Team 2",
+                "team2": "ORGA",
                 "result": "0:0",
                 "rounds": "0-0",
+                "game": 1
             },
             {
                 "team1": "Team 3",
                 "team2": "Team 4",
                 "result": "0:0",
                 "rounds": "0-0",
+                "game": 2
             },
             {
                 "team1": "Team 5",
                 "team2": "Team 6",
                 "result": "0:0",
                 "rounds": "0-0",
+                "game": 3
             },
             {
                 "team1": "Team 7",
                 "team2": "Team 8",
                 "result": "0:0",
                 "rounds": "0-0",
+                "game": 4
             }
         ],
-        "Runde 2":[],
-        "Runde 3":[]
+        "Runde 2": [],
+        "Runde 3": [],
+        "Runde 5": {
+            "team1": "Team 7",
+            "team2": "Team 8",
+            "result": "0:0",
+            "rounds": "0-0",
+            "game": 11
+        },
+        "Runde 6": {
+            "team1": "Team 7",
+            "team2": "Team 8",
+            "result": "0:0",
+            "rounds": "0-0",
+            "game": 12
+        },
+        "Runde 7": {
+            "team1": "Team 7",
+            "team2": "Team 8",
+            "result": "0:0",
+            "rounds": "0-0",
+            "game": 13
+        },
+        "Runde 8": {
+            "team1": "Team 7",
+            "team2": "Team 8",
+            "result": "0:0",
+            "rounds": "0-0",
+            "game": 14
+        },
+        "Runde 9": {
+            "team1": "Team 7",
+            "team2": "Team 8",
+            "result": "0:0",
+            "rounds": "0-0",
+            "game": 15
+        },
+        "Runde 10": {
+            "team1": "LALA",
+            "team2": "LOPLOP",
+            "result": "0:0",
+            "rounds": "0-0",
+            "game": 16
+        }
+
 
     };
+    selectMatch = {};
+    userClanName = [];
+    userAdminId = '';
 
 
     tournament; // The whole tournament
@@ -128,7 +179,8 @@ export class SwissComponent implements OnInit {
     constructor(private _tournamentService: BaTournamentService,
                 private route: ActivatedRoute,
                 public fb: FormBuilder,
-                private _toastService: NotificationsService) {
+                private _toastService: NotificationsService,
+                private _clanService: BaClanService) {
         this.route.params.subscribe(params => {
             this.tournamentId = params['tournamentId'];
         });
@@ -136,6 +188,8 @@ export class SwissComponent implements OnInit {
 
     ngOnInit() {
         this.getTournament(this.tournamentId);
+        this.userAdminId = localStorage.getItem('blackWidow');
+        this.getUserClanName();
         console.log(this.swissMatch);
     }
 
@@ -145,8 +199,7 @@ export class SwissComponent implements OnInit {
             data => {
                 this.tournament = data.obj;
                 this.games = data.obj.games;
-
-
+                console.log(data);
             },
             // the second argument is a function which runs on error
             err => console.error(err),
@@ -155,8 +208,41 @@ export class SwissComponent implements OnInit {
         );
     }
 
-    openMatchInfo(match){
+    getUserClanName() {
+        this._clanService.getClanList().subscribe(
+            // the first argument is a function which runs on success
+            data => {
+                for (let key in data.obj) {
+                    this.userClanName[key] = data.obj[key].name;
+                }
+            },
+            // the second argument is a function which runs on error
+            err => console.error(err),
+            // the third argument is a function which runs on completion
+            () => console.log('loading clan')
+        );
+    }
+
+    openMatchInfo(match) {
+        let splitResult;
+
+        for (let userClan of this.userClanName) {
+            if (userClan == match.team1 || userClan == match.team2) {
+                match["isInClan"] = true;
+                break;
+            } else {
+                match["isInClan"] = false;
+            }
+        }
+
+
+        splitResult = match.result.split(":");
+        match["resultT1"] = splitResult[0];
+        match["resultT2"] = splitResult[1];
+
         this.match.open();
+        this.selectMatch = match;
+        console.log(this.selectMatch);
     }
 
 }
