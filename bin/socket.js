60a8babc624ba3e94b1f0098be3e2ed66283e63d
Dirk Müller <geninone@gmail.com>
2017-03-22 11:18:02 +0100

    bug Fixes for the Chat for Secure

--- bin/socket.js
+++ bin/socket.js
@@ -1,33 +1,27 @@
-var socketIo = require('socket.io');
-var io = socketIo();
-var chat = {};
+module.exports = function (io) {
+    'use strict';
+    io.set("origins", "*:*");
+    io.on('connection', function (socket) {
+        // Send message
+        socket.on('newMessage', function (data) {
+            socket.emit('chatUpdate', data);
+            socket.broadcast.emit('chatUpdate', data);
+        });
 
-chat.io = io;
+        // check if new user is join
+        socket.on('newUser', function (data) {
+            socket.emit('chatUpdate',
+                {'userName': '', 'text': data + ' ist dem Chat beigetreten'});
+            socket.broadcast.emit('chatUpdate',
+                {'userName': '', 'text': data + ' ist dem Chat beigetreten'});
+        });
 
-io.set("origins", "*:*");
-
-io.on('connection', function (socket) {
-    // Send message
-    socket.on('newMessage', function (data) {
-        socket.emit('chatUpdate',data);
-        socket.broadcast.emit('chatUpdate',data);
+        // check if user is leave
+        socket.on('leaveUser', function (data) {
+            socket.emit('chatUpdate',
+                {'userName': '', 'text': data + ' hat den Chat verlassen'});
+            socket.broadcast.emit('chatUpdate',
+                {'userName': '', 'text': data + ' hat den Chat verlassen'});
+        });
     });
-
-    // check if new user is join
-    socket.on('newUser', function (data) {
-        socket.emit('chatUpdate',
-            {'userName':'','text':data+' ist dem Chat beigetreten'});
-        socket.broadcast.emit('chatUpdate',
-            {'userName':'','text':data+' ist dem Chat beigetreten'});
-    });
-
-    // check if user is leave
-    socket.on('leaveUser', function (data) {
-        socket.emit('chatUpdate',
-            {'userName':'','text':data+' hat den Chat verlassen'});
-        socket.broadcast.emit('chatUpdate',
-            {'userName':'','text':data+' hat den Chat verlassen'});
-    });
-});
-
-module.exports = chat;
+};
