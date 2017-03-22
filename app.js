f79207c1da84d50403193d3ae5d62aa4bd3e4443
Dirk Müller <geninone@gmail.com>
2017-03-22 10:15:29 +0100

    bug Fixes for the Chat for Secure

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ var adminRoutes = require('./routes/admin');
 var regRoutes = require('./routes/reg');
 
 var app = express();
-var socketServer = require('./bin/socket');
 
-socketServer.start();
 mongoose.connect('localhost:27017/kjgaming');
 
 // view engine setup
--- bin/socket.js
+++ bin/socket.js
@@ -1,38 +1,33 @@
-var express = require('express');
-var app = express();
-var http = require('http');
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
+var socketIo = require('socket.io');
+var io = socketIo();
+var chat = {};
 
-var chat = {
-    start: function(){
-        server.listen(8070);
-        io.set("origins", "*:*");
+chat.io = io;
 
-        io.on('connection', function (socket) {
-            // Send message
-            socket.on('newMessage', function (data) {
-                socket.emit('chatUpdate',data);
-                socket.broadcast.emit('chatUpdate',data);
-            });
+io.set("origins", "*:*");
 
-            // check if new user is join
-            socket.on('newUser', function (data) {
-                socket.emit('chatUpdate',
-                    {'userName':'','text':data+' ist dem Chat beigetreten'});
-                socket.broadcast.emit('chatUpdate',
-                    {'userName':'','text':data+' ist dem Chat beigetreten'});
-            });
+io.on('connection', function (socket) {
+    // Send message
+    socket.on('newMessage', function (data) {
+        socket.emit('chatUpdate',data);
+        socket.broadcast.emit('chatUpdate',data);
+    });
 
-            // check if user is leave
-            socket.on('leaveUser', function (data) {
-                socket.emit('chatUpdate',
-                    {'userName':'','text':data+' hat den Chat verlassen'});
-                socket.broadcast.emit('chatUpdate',
-                    {'userName':'','text':data+' hat den Chat verlassen'});
-            });
-        });
-    }
-};
+    // check if new user is join
+    socket.on('newUser', function (data) {
+        socket.emit('chatUpdate',
+            {'userName':'','text':data+' ist dem Chat beigetreten'});
+        socket.broadcast.emit('chatUpdate',
+            {'userName':'','text':data+' ist dem Chat beigetreten'});
+    });
+
+    // check if user is leave
+    socket.on('leaveUser', function (data) {
+        socket.emit('chatUpdate',
+            {'userName':'','text':data+' hat den Chat verlassen'});
+        socket.broadcast.emit('chatUpdate',
+            {'userName':'','text':data+' hat den Chat verlassen'});
+    });
+});
 
 module.exports = chat;
